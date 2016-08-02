2587ef3336f5960fb6015d85b58be0df21b5ea1e
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-02 16:43:05 -0400

    unified max_size

--- tests/test_primers.py
+++ tests/test_primers.py
@@ -47,8 +47,8 @@ def test_gibson_assembly_primers(part1, part2):
     assert part1_re[15:].reverse_complement() in Seq(part1)
 
 
-@given(text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=1000),
-       text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=1000))
+@given(text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500),
+       text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500))
 def test_gibson_sequencing_primers(part1, part2):
     assume(shannon_entropy(part1) > 0.24)
     assume(shannon_entropy(part2) > 0.24)
