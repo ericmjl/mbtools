8e61fae1729e71a0230cf5c5381b4591da476c2e
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-02 16:32:51 -0400

    speed optimization fixes

--- tests/test_primers.py
+++ tests/test_primers.py
@@ -1,24 +1,25 @@
-from mbtools.primers import anneal_temp, gibson_assembly_primers
+from mbtools.primers import (anneal_temp,
+                             gibson_assembly_primers,
+                             gibson_sequencing_primers)
 from Bio.SeqUtils.MeltingTemp import Tm_GC
 from Bio.Seq import Seq
 from hypothesis import given, assume
 from hypothesis.strategies import floats, text
 from entropy import shannon_entropy
+import numpy as np
 
 
 def test_anneal_temp():
     sequence = 'atcgctacgcggctagcgtctagcat'
-
     tm = Tm_GC(sequence)
-
     ta = anneal_temp(sequence)
-
     assert ta == tm - 3
 
 
 @given(floats(min_value=0.01, max_value=0.1),
        text(alphabet=['A', 'T', 'G', 'C'], min_size=15, max_size=60))
 def test_anneal_temp_salt_corr(Na, sequence):
+    assume(shannon_entropy(sequence) > 0.24)
     tm = Tm_GC(sequence)
     ta = anneal_temp(sequence, salt_corr={'Na': Na})
 
@@ -34,8 +35,8 @@ def test_anneal_temp_chem_corr(DMSO, sequence):
     assert ta < tm
 
 
-@given(text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=1000),
-       text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=1000))
+@given(text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500),
+       text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500))
 def test_gibson_assembly_primers(part1, part2):
     assume(shannon_entropy(part1) > 0.24)
     assume(shannon_entropy(part2) > 0.24)
