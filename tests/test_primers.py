2ca9e3a2880b64d7fe17f45a5705d81631f9bb08
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-22 13:12:51 -0400

    updated tests to use seqgenerator

--- tests/test_primers.py
+++ tests/test_primers.py
@@ -6,52 +6,56 @@ from Bio.Seq import Seq
 from hypothesis import given, assume
 from hypothesis.strategies import floats, text
 from entropy import shannon_entropy
+from random import choice
+from .testutils import seq_generator
 import numpy as np
 
 
 def test_anneal_temp():
-    sequence = 'atcgctacgcggctagcgtctagcat'
+    sequence = seq_generator(26)
     tm = Tm_GC(sequence)
     ta = anneal_temp(sequence)
     assert ta == tm - 3
 
 
-@given(floats(min_value=0.01, max_value=0.1),
-       text(alphabet=['A', 'T', 'G', 'C'], min_size=15, max_size=60))
-def test_anneal_temp_salt_corr(Na, sequence):
-    assume(shannon_entropy(sequence) > 0.24)
+@given(floats(min_value=0.01, max_value=0.1))
+def test_anneal_temp_salt_corr(Na):
+    sequence = seq_generator(26)
     tm = Tm_GC(sequence)
     ta = anneal_temp(sequence, salt_corr={'Na': Na})
 
     assert ta < tm
 
 
-@given(floats(min_value=1, max_value=20),
-       text(alphabet=['A', 'T', 'G', 'C'], min_size=15, max_size=60))
-def test_anneal_temp_chem_corr(DMSO, sequence):
+@given(floats(min_value=1, max_value=20))
+def test_anneal_temp_chem_corr(DMSO):
+    sequence = seq_generator(26)
     tm = Tm_GC(sequence)
     ta = anneal_temp(sequence, chem_corr={'DMSO': DMSO})
 
     assert ta < tm
 
 
-@given(text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500),
-       text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500))
-def test_gibson_assembly_primers(part1, part2):
-    assume(shannon_entropy(part1) > 0.24)
-    assume(shannon_entropy(part2) > 0.24)
+def test_gibson_assembly_primers():
+    part1 = seq_generator(500)
+    part2 = seq_generator(300)
 
-    part2_fw, part1_re = gibson_assembly_primers(part1, part2)
+    primer_len = 40
+    part2_fw, part1_re = gibson_assembly_primers(part1, part2,
+                                                 primer_length=primer_len)
 
-    assert part2_fw[15:] in Seq(part2)
-    assert part1_re[15:].reverse_complement() in Seq(part1)
+    ohl = int(round(primer_len * 1 / 3))
+    ahl = primer_len - ohl
+
+    assert part2_fw[0:ohl] == Seq(part1)[-ohl:]
+    assert part2_fw[ohl:] in Seq(part2)
+    assert part1_re.reverse_complement()[-ohl:] == Seq(part2)[0:ohl]
+    assert part1_re.reverse_complement()[0:ahl] == Seq(part1)[-ahl:]
 
 
-@given(text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500),
-       text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500))
-def test_gibson_sequencing_primers(part1, part2):
-    assume(shannon_entropy(part1) > 0.24)
-    assume(shannon_entropy(part2) > 0.24)
+def test_gibson_sequencing_primers():
+    part1 = seq_generator(300)
+    part2 = seq_generator(900)
 
     up_3p, dn_5p = gibson_sequencing_primers(part1, part2)
     assert str(up_3p) in part1
