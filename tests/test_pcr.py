3a89bb5eea4006c9c7893839a42efc496e0b0839
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-22 13:11:50 -0400

    made overhang test more general

--- tests/test_pcr.py
+++ tests/test_pcr.py
@@ -20,19 +20,14 @@ def test_amplify_without_overhang():
 
 
 def test_amplify_with_overhang():
-    """
-    This test is designed as an oracle test rather than a Hypothesis test as
-    above. This is because I have yet to be able to write a Hypothesis-version
-    that works.
-    """
-    template = 'ACTACTCGCGGCATCTAGCTCACGTGACTACGTCGCGCATGGTCGTACTGTACGTCTGA' +\
-               'CTATGCATCGTGTGTATTTGCGTTGGTCGTCGGCGTCATCGTAGTATTCTCTTTAGC'
-    fw_oh = 'CGATTCGG'
-    re_oh = 'GCGTCTTA'
+    template = seq_generator(200)
+    primer_length = 60
+    ohl = 15
+    fw_oh = seq_generator(ohl)
+    re_oh = seq_generator(ohl)
 
-    fw_primer = Seq(fw_oh + template[0:60 - len(fw_oh)])
-    re_primer = Seq(template[-60 + len(re_oh):] + re_oh).reverse_complement()
-    template = Seq(template)
+    fw_primer = Seq(fw_oh + template[0:primer_length-ohl])
+    re_primer = Seq(template[-primer_length+ohl:] + re_oh).reverse_complement()
 
     amplicon = compute_amplicon(fw_primer, re_primer, template)
 
