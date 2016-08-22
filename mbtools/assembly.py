4165d377f44287432d432b23cc36d3865188aa5b
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-22 13:10:18 -0400

    added type checking for assembly, added utility funcs

--- mbtools/assembly.py
+++ mbtools/assembly.py
@@ -1,31 +1,53 @@
 import networkx as nx
 from .primers import gibson_assembly_primers, gibson_sequencing_primers
+from .pcr import compute_amplicon
+from Bio.SeqRecord import SeqRecord
+from collections import defaultdict
 
 
 class GibsonAssembler(nx.DiGraph):
+    """
+    Inherits nx.DiGraph.
+
+    Graph definition:
+    - nodes: BioPython Seq objects
+        - id: seqrecord ID
+        - obj: SeqRecord object
+    - edges: the order in which parts are to be assembled.
+    """
     def __init__(self, sequences):
         super(GibsonAssembler, self).__init__()
 
-        self.sequences = sequences
+        self.add_sequences(sequences)
         self.add_nodes()
         self.add_edges()
         self.compute_assembly_primers()
         self.compute_sequencing_primers()
 
+    def add_sequences(self, sequences):
+        self.sequences = []
+        for s in sequences:
+            assert isinstance(s, SeqRecord),\
+                "sequences must be an iterable of BioPython SeqRecord objects."
+            self.sequences.append(s)
+
     def add_nodes(self):
-        for s in self.sequences:
-            self.add_node(s)
+        for i, s in enumerate(self.sequences):
+            self.add_node(s.seq, id=s.id, obj=s)
 
     def add_edges(self):
-        for i in range(len(self.sequences)):
+        for i, s in enumerate(self.sequences):
             if i < len(self.sequences) - 1:
-                self.add_edge(self.sequences[i], self.sequences[i+1])
+                self.add_edge(s.seq,
+                              self.sequences[i+1].seq)
             else:
-                self.add_edge(self.sequences[i], self.sequences[0])
+                self.add_edge(s.seq,
+                              self.sequences[0].seq)
 
     def compute_assembly_primers(self):
         for up, dn in self.edges():
             dn_fw, up_re = gibson_assembly_primers(up, dn)
+
             self.node[dn]['fw_gibson'] = dn_fw
             self.node[up]['re_gibson'] = up_re
 
@@ -34,3 +56,26 @@ class GibsonAssembler(nx.DiGraph):
             up_3p, dn_5p = gibson_sequencing_primers(up, dn)
             self.node[up]['3p_sequencing'] = up_3p
             self.node[dn]['5p_sequencing'] = dn_5p
+
+    def primers(self):
+        """
+        Returns a dictionary of part ID and primers.
+        """
+        primers = defaultdict(dict)
+        for n, d in self.nodes(data=True):
+            for p in ['fw_gibson', 're_gibson',
+                      '3p_sequencing', '5p_sequencing']:
+                primers[d['id']][p] = d[p]
+        return primers
+
+    def pcr_products(self):
+        """
+        Returns a dictionary of part ID and PCR product sequences.
+        """
+        pcr_products = dict()
+        for n, d in self.nodes(data=True):
+            pcr_products[d['id']] = compute_amplicon(d['fw_gibson'],
+                                                     d['re_gibson'],
+                                                     n)
+
+        return pcr_products
--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -3,24 +3,33 @@ from hypothesis import given, assume
 from hypothesis.strategies import lists, text
 from mbtools.assembly import GibsonAssembler
 from entropy import shannon_entropy
+from .testutils import seq_generator
+
+# @given(lists(seq_generator(500),
+#              min_size=2,
+#              average_size=3))
 
 
-@given(lists(text(alphabet=['A', 'T', 'G', 'C'], min_size=200, max_size=500),
-             min_size=2,
-             average_size=3))
-def test_gibson_assembly_class(parts):
+def test_gibson_assembly_class():
     """
     Most of the tests here are "data integrity" tests. The structure of the
     GibsonAssembler class has to be done right.
     """
+    parts = [seq_generator(500) for i in range(3)]
+
     g = GibsonAssembler(parts)
     assume(len(set(parts)) > 1)  # make sure no duplicates exist
     for part in parts:
         assume(shannon_entropy(part) > 0.24)
 
+    primer_names = ['fw_gibson', 're_gibson', '3p_sequencing', '5p_sequencing']
     for n, d in g.nodes(data=True):
-        assert set(d.keys()) == set(['fw_gibson', 're_gibson',
-                                     '3p_sequencing', '5p_sequencing'])
+        assert len(set(d.keys()).intersection(primer_names)) == 4
 
     assert len(g.nodes()) == len(g.edges())
     assert len(g.nodes()) == len(g.sequences)
+
+    p = g.primers()
+    assert len(p) == len(g.nodes())
+    for part, primers in p.items():
+        assert len(primers) == 4
