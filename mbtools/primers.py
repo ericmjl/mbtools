60452879b3b0eea9bb06851e8ef15cc2d97ccf5e
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-22 13:10:55 -0400

    changed primer length to 60 default

--- mbtools/primers.py
+++ mbtools/primers.py
@@ -31,19 +31,29 @@ def anneal_temp(sequence, salt_corr=None, chem_corr=None):
     return tm + correction - 3
 
 
-def gibson_assembly_primers(up, dn, primer_length=40):
+def gibson_assembly_primers(up, dn, primer_length=60):
     """
     Computes the necessary Gibson assembly primers for the junction between
     upstream (up) and downstream (dn) parts.
+
+    `up` and `dn` should be BioPython Seq objects
     """
+
+    assert isinstance(up, Seq)
+    assert isinstance(dn, Seq)
+
     anl = int(round(primer_length * 2/3))  # anneal length
     ohl = int(round(primer_length * 1/3))  # overhang length
 
     # Downstream part's fw-assembly primer
-    dn_fw = Seq(up[-ohl:] + dn[0:anl], alphabet=DNAAlphabet())
+    dn_fw = up[-ohl:] + dn[0:anl]
+    # print(up[-ohl:])
+    assert dn_fw[-anl:] == dn[0:anl]
+    assert dn_fw[0:ohl] == up[-ohl:]
     # Upstream part's re-assembly primer
-    up_re = Seq(up[-anl:] + dn[0:ohl], alphabet=DNAAlphabet())\
-        .reverse_complement()
+    up_re = (up[-anl:] + dn[0:ohl]).reverse_complement()
+    assert up_re[0:ohl].reverse_complement() == dn[0:ohl]
+    assert up_re[-anl:].reverse_complement() == up[-anl:]
 
     return dn_fw, up_re
 
@@ -54,8 +64,10 @@ def gibson_sequencing_primers(up, dn, primer_length=20):
     assembled correctly.
     """
     # Upstream part's 3' junction sequencing primer
-    up_3p = Seq(up[-100 - primer_length: - 100])
+    up_3p = up[-100 - primer_length: - 100]
+    assert up_3p in up
     # Downstream part's 5' junction sequencing primer.
-    dn_5p = Seq(dn[100: 100 + primer_length])
+    dn_5p = dn[100: 100 + primer_length].reverse_complement()
+    assert dn_5p.reverse_complement() in dn
 
     return up_3p, dn_5p
