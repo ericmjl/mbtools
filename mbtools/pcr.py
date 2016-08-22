6403bd3ad9971cc97ebab9cbd57fd8b4fcc7b402
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-22 13:10:42 -0400

    bug fix on alignment_indices and other fixes

--- mbtools/pcr.py
+++ mbtools/pcr.py
@@ -8,10 +8,12 @@ from Levenshtein import distance
 from difflib import SequenceMatcher
 from Bio.Seq import Seq
 
+import math
+import numpy as np
 
-def alignment_indices(str1, str2):
+def alignment_indices(template, primer):
     """
-    Finds the optimal alignment between two strings.
+    Finds the optimal alignment between template and primer.
 
     Inputs:
     =======
@@ -24,9 +26,8 @@ def alignment_indices(str1, str2):
     For the DNA case, we are assuming that the 5'->3' directionality of the
     two strings are identical.
     """
-    s = SequenceMatcher(None, str1, str2)
-    m = s.find_longest_match(0, len(str1), 0, len(str2))
-
+    s = SequenceMatcher(None, template, primer)
+    m = s.find_longest_match(0, len(template), 0, len(primer))
     return (m.a, m.a + m.size), (m.b, m.b + m.size)
 
 
@@ -36,8 +37,8 @@ def compute_amplicon(fw_primer, re_primer, template):
 
     Inputs:
     =======
-    - fw_primer, re_primer: BioPython SeqRecord objects
-    - template: BioPython SeqRecord objects
+    - fw_primer, re_primer: BioPython Seq objects
+    - template:             BioPython Seq objects
 
     It is assumed that the reverse primer is not in the same orientation as the
     template sequence.
@@ -68,14 +69,57 @@ def compute_amplicon(fw_primer, re_primer, template):
     # rp   : reverse primer
     # tpl  : template
     # idxs : indices
-    fp_idxs, fp_tpl_idxs = alignment_indices(fw_primer,
-                                             template)
-    rp_idxs, rp_tpl_idxs = alignment_indices(re_primer.reverse_complement(),
-                                             template)
+    fp_tpl_idxs, fp_idxs = alignment_indices(template,
+                                             fw_primer)
+    rp_tpl_idxs, rp_idxs = alignment_indices(template,
+                                             re_primer.reverse_complement())
 
-    final_sequence = \
-        fw_primer +\
-        template[fp_tpl_idxs[1]:rp_tpl_idxs[0]] +\
+    final_sequence = fw_primer + template[fp_tpl_idxs[1]:rp_tpl_idxs[0]] +\
         re_primer.reverse_complement()
 
     return final_sequence
+
+
+def num_cycles(fold_amp):
+    """
+    Computes the necessary number of cycles. Rounds up to the next integer.
+    """
+    return math.ceil(np.log10(fold_amp) / np.log10(2))
+
+
+def num_cycles(fold_amp):
+    """
+    Computes the necessary number of cycles. Rounds up to the next integer.
+    """
+    return math.ceil(np.log10(fold_amp) / np.log10(2))
+
+
+def input_plasmid_mass(target_len, plasmid_len, target_mass):
+    """
+    Computes the mass of total input plasmid mass required to get a given mass
+    of target DNA.
+
+    Silently assumes that the units are:
+    - target_len: bp (base pairs)
+    - plasmid_len: bp (base pairs)
+    - target_mass: ng (nanograms)
+    """
+    return target_mass * plasmid_len / target_len
+
+
+def input_volume(input_mass, input_conc):
+    """
+    Computes the required amount of volume for a given mass and concentration.
+
+    Silently assumes that the units are:
+    - input_mass: ng (nanograms)
+    - input_conc: ng/µL (nanograms per microlitre)
+    """
+    return input_mass / input_conc
+
+
+def num_cycles(fold_amp):
+    """
+    Computes the necessary number of cycles. Rounds up to the next integer.
+    """
+    return math.ceil(np.log10(fold_amp) / np.log10(2))
