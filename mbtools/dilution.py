3d31d86b3a042920e8f3c2ab6a3defaaf3c04fe5
Vivian Zhong <vivzhong@mit.edu>
2016-09-08 15:57:59 -0400

    committed cpec_equimolarity function

--- mbtools/dilution.py
+++ mbtools/dilution.py
@@ -24,3 +24,30 @@ def input_plasmid_mass(target_len, plasmid_len, target_mass):
     - target_mass: ng (nanograms)
     """
     return target_mass * plasmid_len / target_len
+
+def cpec_equimolarity(dict_conc_bp):
+    """
+    For every sequence, the key is the name of the sequence, and the value is a list of
+    two numbers: 1) concentration of DNA in solution (g/ul) and
+    2) the number of base pairs in the sequence
+    """
+    d_concentrations = {}
+    l_concentrations = []
+    relvol = {}
+    for key, value in dict_conc_bp.items():
+        #Calculate the concentration of fragments of a particular sequence in solution
+        fragmentperul = (float(value[0])/(1.62*10**-21))/float(value[1])
+
+        d_concentrations[key] = fragmentperul
+        l_concentrations.append(fragmentperul)
+    for key, value in d_concentrations.items():
+        """
+        By dividing the highest concentration by each of the other concentrations,
+        we know how much volume of a given sequence needs to used to make it equimolar to
+        one unit volume of the sequence with the highest concentration
+        """
+
+        volume = float(max(l_concentrations))/float(value)
+        relvol[key] = volume
+
+    return relvol
